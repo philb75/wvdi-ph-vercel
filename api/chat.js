7971c5e2dab15949e552ea1106ef3ca32f33f3b1
Philippe Barthelemy <philb75@gmail.com>
2025-04-27 00:09:17 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,19 +1,5 @@
 import OpenAI from 'openai';
 
-export default async (req, res) => {
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', 'https://western-visayas-driving-institute.github.io');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    return res.status(204).end();
-  }
-
-  // … OpenAI call …
-
-  res.setHeader('Access-Control-Allow-Origin', 'https://western-visayas-driving-institute.github.io');
-  res.status(200).json({ reply });
-};
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
